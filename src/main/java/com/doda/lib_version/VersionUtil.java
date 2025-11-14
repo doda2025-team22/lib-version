33c694efb1f431cf9155f97cd2d1e3c6c3685113
Ada Turgut <a.turgut@student.tudelft.nl>
2025-11-14 18:34:42 +0100

    remove useless directory and add util class

---
+++ src/main/java/com/doda/lib_version/VersionUtil.java
@@ -0,0 +1,20 @@
+package com.doda.lib_version;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class VersionUtil {
+
+    public String getVersion() {
+        Properties props = new Properties();
+        InputStream resourceStream = VersionUtil.class.getResourceAsStream("/version.properties");
+        try {
+            props.load(resourceStream);
+            return props.getProperty("version");
+        } catch (IOException e) {
+            System.out.println("Error in loading properties");
+            throw new RuntimeException(e);
+        }
+    }
+}
